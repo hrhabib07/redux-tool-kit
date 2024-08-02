74188dd21a8ddb25908b56441e88924ba11f64c9
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-08-02 23:47:00 +0600

    counter app design implemented

--- src/features/counter/CounterView.tsx
+++ src/features/counter/CounterView.tsx
@@ -1,20 +1,44 @@
-import { useDispatch } from "react-redux";
-import { useAppSelector } from "../../app/hooks";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { decrement, increaseByAmount, increment, reset } from "./counterSlice";
 
 const CounterView = () => {
   const counter = useAppSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const amount = 5;
   return (
-    <div>
-      <h1>Counter : {counter}</h1>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-      <button onClick={() => dispatch(increaseByAmount(amount))}>
-        Increase by {amount}
-      </button>
-      <button onClick={() => dispatch(reset())}>Reset</button>
+    <div className="max-w-7xl mx-auto flex justify-center h-screen items-center">
+      <div>
+        <h2 className="text-5xl font-bold text-green-900">My counter app</h2>
+        <h1 className="text-4xl font-semibold my-8">
+          Counter : <span className="text-red-900">{counter}</span>{" "}
+        </h1>
+        <div className="grid grid-cols-2 gap-4">
+          <button
+            className="p-2 bg-green-700 text-white rounded-md  mr-8 text-lg uppercase"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <button
+            className="p-2 bg-red-700 text-white rounded-md  mr-8 text-lg uppercase"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+          <button
+            className="p-2 bg-green-700 text-white rounded-md  mr-8 text-lg uppercase"
+            onClick={() => dispatch(increaseByAmount(amount))}
+          >
+            Increase by {amount}
+          </button>
+          <button
+            className="p-2 bg-red-700 text-white rounded-md  mr-8 text-lg uppercase"
+            onClick={() => dispatch(reset())}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
