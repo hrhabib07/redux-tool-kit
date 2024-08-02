4978c476533abd03ebd4004bd743abcefb2b0c0a
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-08-02 23:26:55 +0600

    action payload added on the counter app

--- src/features/counter/CounterView.tsx
+++ src/features/counter/CounterView.tsx
@@ -1,15 +1,19 @@
 import { useDispatch } from "react-redux";
 import { useAppSelector } from "../../app/hooks";
-import { decrement, increment, reset } from "./counterSlice";
+import { decrement, increaseByAmount, increment, reset } from "./counterSlice";
 
 const CounterView = () => {
   const counter = useAppSelector((state) => state.counter.count);
   const dispatch = useDispatch();
+  const amount = 5;
   return (
     <div>
       <h1>Counter : {counter}</h1>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(increaseByAmount(amount))}>
+        Increase by {amount}
+      </button>
       <button onClick={() => dispatch(reset())}>Reset</button>
     </div>
   );
